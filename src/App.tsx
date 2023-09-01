877d67bb21655137fa946337a068c17a4b4b4092
ElizabethArr <elizaarroyoe@gmail.com>
2023-08-31 18:40:09 -0600

    se agrego useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-
+import { Counter } from './components/Counter';
+import { Usuario } from  './components/Usuario';
 
 function App() {
   return (
     <>
       <h1>Hola mundo - Ts </h1>
+      <hr/>
+    
+    <Counter />
+
+    <Usuario />
+
+
     </>
   );
 }
